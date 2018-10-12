68caf5b54ebf5b095dca0a07b5d400ed78a40117
dineshvaran <gdineshvaran@gmail.com>
2018-10-12 23:41:50 +0530

    action bar menu items added for detail layout

--- app/src/main/java/varanz/android/application/irctcticketreminder/DetailActivity.java
+++ app/src/main/java/varanz/android/application/irctcticketreminder/DetailActivity.java
@@ -3,6 +3,10 @@ package varanz.android.application.irctcticketreminder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.util.zip.Inflater;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detial_actionbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
